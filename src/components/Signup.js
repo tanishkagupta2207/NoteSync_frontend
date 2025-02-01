35d6d20b60366623097d0f243ec2b8fd78a0e957
tanishkagupta2207 <tanishkagupta2207@gmail.com>
2025-02-02 00:39:23 +0530

    handle Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom';
 
 const Signup = () => {
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    cpassword: ''
+  });
+  let navigate = useNavigate();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(credentials.password !== credentials.cpassword){
+      alert('Password and Confirm Password do not match!');
+      return ;
+    }
+    const response = await fetch(`${process.env.REACT_APP_HOST_URL}api/auth/createUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    }); 
+    const res = await response.json();
+    console.log(res); 
+    if(res.success){
+      localStorage.setItem("token", res.authToken);
+      navigate('/');
+    }
+    else{
+      alert('Could not SignUp!');
+    }
+  }
+  
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div>
-      
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            required
+            name="name"
+            onChange={onChange}
+            value={credentials.name}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            required
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            value={credentials.email}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password"
+            onChange={onChange}
+            required
+            minLength={5}
+            value={credentials.password}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="cpassword"
+            className="form-control"
+            id="cpassword"
+            onChange={onChange}
+            required
+            minLength={5}
+            value={credentials.cpassword}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
